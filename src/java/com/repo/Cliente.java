ae93b99f6547c035531e45f0f1d4e60f31204111
Xavier <g_morales@yahoo.es>
2018-05-06 14:40:03 -0600

    PrimerComit

---
+++ src/java/com/repo/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.repo;
+
+/**
+ *
+ * @author javadefensa
+ */
+public class Cliente {
+    private Integer idCliente;
+    private String  nombreCliente;
+    private String  NIT;
+    
+    
+}
